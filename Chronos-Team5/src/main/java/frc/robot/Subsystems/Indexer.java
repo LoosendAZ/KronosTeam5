46dac31ff411461e8c60fe0c246bb62712fddd27
SunYic26 <116052926+SunYic26@users.noreply.github.com>
2024-11-12 20:13:21 -0800

    TALON Fx

--- Chronos-Team5/src/main/java/frc/robot/Subsystems/Indexer.java
+++ Chronos-Team5/src/main/java/frc/robot/Subsystems/Indexer.java
@@ -4,12 +4,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase{
     private Indexer s_Indexer;
+    
     public Indexer getInstance() {
         if(s_Indexer == null) {
             s_Indexer = new Indexer();
         }
         return s_Indexer;
     }
+
     public Indexer() {
 
     }
