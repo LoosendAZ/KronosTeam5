b44b785d8389c2ea6268cafb6342eec18bfe0f04
SunYic26 <116052926+SunYic26@users.noreply.github.com>
2024-12-17 20:31:50 -0800

    lock in

--- Chronos-Team5/src/main/java/frc/robot/RobotContainer.java
+++ Chronos-Team5/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Commands.StartIntake;
 import frc.robot.Subsystems.*;
@@ -57,7 +58,14 @@ public class RobotContainer {
       lTrigger = new Trigger(() -> controller.getLeftBumper());
       rTrigger = new Trigger(() -> controller.getRightBumper());
 
-      aButton.onTrue(s_Indexer.setSpeed(IndexerStates.ON));
+
+      //placeholders; research actual command configuration
+      aButton.onTrue(getAutonomousCommand());
+      bButton.whileTrue(getAutonomousCommand());
+      xButton.whileTrue(getAutonomousCommand());
+      yButton.onTrue(getAutonomousCommand());
+      
+
   }
 
   
